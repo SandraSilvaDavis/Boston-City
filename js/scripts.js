64e67c6adc70be66d5bad2fd536208d055d3540a
Sandra Davis <sandra.silva.davis@gmail.com>
2023-04-27 22:39:33 -0600

    change

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
-const swiper = new Swiper('.swiper', {
+const hb = document.querySelector(`#hamburgerBtn`);
+
+
+hb.addEventListener(`click`, () => {
+  document.querySelector(`#primaryNav`).classList.toggle(`open`);
+})
+
+
+
+//const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'horizontal',
-    speed: 2000,
-    loop: true,
+   // direction: 'horizontal',
+    //speed: 2000,
+    //loop: true,
   
     // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+   // pagination: {
+      //el: '.swiper-pagination',
       //Enable Auto Play
-      autoplay: {
-        delay: 5000,
-      },
-    },
-});
+    //  autoplay: {
+      //  delay: 5000,
+   //   },
+   // },
+//});//
