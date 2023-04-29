b7d1d36c4daa4fd645f8ae187dc728f20dd83d4b
Sandra Davis <sandra.silva.davis@gmail.com>
2023-04-28 21:22:17 -0600

    added pictures

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,20 @@ hb.addEventListener(`click`, () => {
 
 
 
-//const swiper = new Swiper('.swiper', {
-    // Optional parameters
-   // direction: 'horizontal',
-    //speed: 2000,
-    //loop: true,
-  
-    // If we need pagination
-   // pagination: {
-      //el: '.swiper-pagination',
-      //Enable Auto Play
-    //  autoplay: {
-      //  delay: 5000,
-   //   },
-   // },
-//});//
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  speed: 2000, 
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Enable autoplay 
+  autoplay: {
+    delay: 5000, 
+  },
+
+});
